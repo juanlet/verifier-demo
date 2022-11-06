939940431e5adfa1dd9be6ed896b5e5ca81e45ed
Juan Letamendia <jletamendia@f4.co>
2022-11-05 23:49:48 -0300

    Added media queries

--- src/containers/SuccessPage.tsx
+++ src/containers/SuccessPage.tsx
@@ -1,12 +1,12 @@
 import Button from "@/components/Button"
 import styles from "@/containers/SuccessPage.module.css"
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
 export default function SuccessPage() {
-    const navigate = useNavigate();
+    const navigate = useNavigate()
     return (
-        <div className={styles.container}>
+        <div className={styles.Container}>
             <h1>Congratulations</h1>
-            <p style={{ marginBottom: '1rem' }}>You have successfully completed the verification process.</p>
+            <div style={{ marginBottom: '1rem' }} className={styles.SuccessMessage}>You have successfully completed the verification process.</div>
             <Button onClick={() => navigate("/")}>Go back</Button>
         </div>
     )
